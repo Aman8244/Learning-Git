36b1d0c268a379a7ccbf043be2fd200d6dbbe91a
Aman Singh <amansinghmarch16@gmail.com>
2022-12-03 23:02:46 +0530

    add code into diff.js

---
+++ diff.js
@@ -0,0 +1,6 @@
+diff = function(a,b){
+    return a-b;
+}
+var num1 = Number(alert("Enter First Number:"));
+var num2 = Number(alert("Enter second Number:"));
+console.log("The difference is: "+diff(num1,num2));
