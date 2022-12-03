683272cdfacfff75c2189c2daa1b97293bf7461e
Aman Singh <amansinghmarch16@gmail.com>
2022-12-03 22:42:11 +0530

    Multiply code added

---
+++ multiply.js
@@ -0,0 +1,6 @@
+multiply = function(a,b){
+    return a*b;
+}
+var num1 = Number(alert("Enter First Number:"));
+var num2 = Number(alert("Enter second Number:"));
+console.log("The sum is: "+multiply(num1,num2));
